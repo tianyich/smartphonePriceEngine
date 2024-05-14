82d9341140efd7c811232cf90363f3d9e52ee31c
Tianyi Chen <t7chen@ucsd.edu>
2024-05-13 23:23:34 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
             appDir: 'app'
         }),
         paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : 'https://github.com/tianyich/smartphonePriceEngine'
 		}
 	}
 };
